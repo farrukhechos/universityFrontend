bff74e1672f6c96ba5eeacdd3f5a3f7bf8adb8f2
Farrukh Ganiboyev <farrukhechos@gmail.com>
2026-02-19 09:03:36 +0500

    Operators

---
+++ JavaScript/2-dars/main.js
@@ -0,0 +1,17 @@
+
+// Variables
+let myName = "Farrux";
+let mySurname = "G'aniboyev";
+let myAge = 18;
+let myUniversityGroup = 209;
+
+
+console.log("Fullname: " + myName + " " + mySurname);
+console.log("Age: " + myAge);
+console.log("University group: " + myUniversityGroup);
+
+if (myAge >= 18){
+    console.log("You are adult!")
+} else{
+    console.log("You are not adult, you are a child. Please leave this website!")
+}
---
+++ JavaScript/2-dars/operators.js
@@ -0,0 +1,30 @@
+
+// Compare operators
+// #1 <
+// #2 >
+// #3 >=
+// #4 <=
+// #5 ==
+// #6 ===
+// #7 !=
+// #9 !==
+
+
+
+
+// assignment operation
+// #1 =
+// #2 =+
+// #3 -=
+// #4 /=
+// #5 *=
+// #6 %=
+
+const plusOne = function(digits) {
+    let numbers= []
+    numbers.push(digits)
+    let newDigs = numbers[numbers.length - 1] + 1
+    console.log(newDigs)
+};
+
+plusOne([1,2,3])
