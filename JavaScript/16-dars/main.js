ecf9b26f820810a6b3c7c51ba75e8cbf6cc4b27b
Farrukh Ganiboyev <farrukhechos@gmail.com>
2026-04-01 01:36:55 +0500

    Today we learned about DOM a little

---
+++ JavaScript/16-dars/main.js
@@ -0,0 +1,7 @@
+    let name = document.getElementById('name');
+    let lastName = document.getElementById('lastname');
+    let patronymic = document.getElementById('patronymic');
+
+    function showFISH(){
+        alert(`${name.value} ${lastName.value} ${patronymic.value}`);
+    }
